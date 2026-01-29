e105f4c565b9b238880d01087f22451d37240bf3
meenakshiphogat07 <meenakshiphogat07@gmail.com>
2026-01-29 22:34:35 +0530

    added new features

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,173 @@
+
+import airfreshner from "./airfreshner.jpeg";
+import bagpack from "./bagpack.jpeg";
+import cutlery from "./cutlery.jpeg";
+import deo from "./deo.jpeg";
+import facewash from "./facewash.jpeg";
+import kitchentowel from "./kitchentowel.jpeg";
+import laptopsleeve from "./laptopsleeve.jpeg";
+import laundry from "./laundry.jpeg";
+import lipbalm from "./lipbalm.jpeg";
+import paperpen from "./paperpen.jpeg";
+import pencil from "./pencil.jpeg";
+import razor from "./razor.jpeg";
+import scrub from "./scrub.jpeg";
+import soap from "./soap.jpeg";
+import straws from "./straws.jpeg";
+import tissue from "./tissue.jpeg";
+import toothbrush from "./toothbrush.jpeg.webp";
+
+export const products = [
+  {
+    _id: "eco1",
+    name: "Eco Friendly Air Freshener",
+    price: 199,
+    image: [airfreshner],
+    category: "Eco",
+    subCategory: "Home",
+    date: 1716668445448,
+    bestseller: true,
+  },
+  {
+    _id: "eco2",
+    name: "Reusable Backpack",
+    price: 999,
+    image: [bagpack],
+    category: "Eco",
+    subCategory: "Lifestyle",
+    date: 1716668445448,
+    bestseller: true,
+  },
+  {
+    _id: "eco3",
+    name: "Reusable Cutlery Set",
+    price: 299,
+    image: [cutlery],
+    category: "Eco",
+    subCategory: "Kitchen",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco4",
+    name: "Natural Deodorant",
+    price: 249,
+    image: [deo],
+    category: "Eco",
+    subCategory: "Personal Care",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco5",
+    name: "Herbal Face Wash",
+    price: 199,
+    image: [facewash],
+    category: "Eco",
+    subCategory: "Skincare",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco6",
+    name: "Reusable Kitchen Towel",
+    price: 149,
+    image: [kitchentowel],
+    category: "Eco",
+    subCategory: "Kitchen",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco7",
+    name: "Laptop Sleeve",
+    price: 399,
+    image: [laptopsleeve],
+    category: "Eco",
+    subCategory: "Accessories",
+    date: 1716668445448,
+    bestseller: false,
+  },
+
+
+  {
+    _id: "eco13",
+    name: "Paper Pen",
+    price: 49,
+    image: [paperpen],
+    category: "Eco",
+    subCategory: "Stationery",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco14",
+    name: "Wooden Pencil",
+    price: 29,
+    image: [pencil],
+    category: "Eco",
+    subCategory: "Stationery",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco15",
+    name: "Reusable Razor",
+    price: 199,
+    image: [razor],
+    category: "Eco",
+    subCategory: "Personal Care",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco16",
+    name: "Natural Scrub",
+    price: 249,
+    image: [scrub],
+    category: "Eco",
+    subCategory: "Skincare",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco17",
+    name: "Organic Soap",
+    price: 99,
+    image: [soap],
+    category: "Eco",
+    subCategory: "Personal Care",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco18",
+    name: "Steel Straws",
+    price: 149,
+    image: [straws],
+    category: "Eco",
+    subCategory: "Kitchen",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco19",
+    name: "Recycled Tissue",
+    price: 79,
+    image: [tissue],
+    category: "Eco",
+    subCategory: "Home",
+    date: 1716668445448,
+    bestseller: false,
+  },
+  {
+    _id: "eco20",
+    name: "Bamboo Toothbrush",
+    price: 89,
+    image: [toothbrush],
+    category: "Eco",
+    subCategory: "Personal Care",
+    date: 1716668445448,
+    bestseller: false,
+  },
+];
